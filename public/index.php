8b496d886afc8e94f2fb05be2883a7a9f96d3ae7
unknown <sr.ska89@gmail.com>
2021-05-03 23:35:13 -0300

    intento 101

--- public/index.php
+++ public/index.php
@@ -1,8 +1,6 @@
 
 <?php
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-
-header('Access-Control-Allow-Origin: *');
+Header('Access-Control-Allow-Origin: *');
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
